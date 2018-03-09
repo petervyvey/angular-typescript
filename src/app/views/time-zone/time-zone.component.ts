3110cb28441b80be6ac3178a45d8c97311abc6de
Peter Vyvey <allphi01@renson.be>
2018-03-09 07:37:54 +0100

    unsubscribe on destroy

--- src/app/views/time-zone/time-zone.component.ts
+++ src/app/views/time-zone/time-zone.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ReplaySubject } from 'rxjs/Rx';
 import { Store } from '@ngrx/store';
 import { TimeZoneDbStoreService } from '@services/time-zone-db-store/time-zone-db-store.module';
 import { IState } from '@store/state';
@@ -10,20 +11,29 @@ import { SetCountries } from '@store/time-zone/actions/set-countries.action';
     templateUrl: './time-zone.component.html',
     styleUrls: ['./time-zone.component.scss']
 })
-export class TimeZoneComponent implements OnInit {
+export class TimeZoneComponent implements OnInit, OnDestroy {
 
     constructor(private store: Store<IState>, private service: TimeZoneDbStoreService) { }
+
+    public destroy$ = new ReplaySubject<boolean>();
 
     public ngOnInit() {
         console.log('store', this.store);
         const state = this.store.select(x => x.timeZone);
 
-        state.subscribe(x => console.log('state', x));
+        state
+        .takeUntil(this.destroy$)
+        .subscribe(x => console.log('state', x));
 
         this.service.getTimeZones()
+            .take(1)
             .map(x => x.zones.map(z => new CountryInfo(z.countryCode, z.countryName)))
-            .do(x => state.dispatch(new SetCountries({countries: x})))
+            .do(x => state.dispatch(new SetCountries({ countries: x })))
             .subscribe();
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next(true);
     }
 
 }
